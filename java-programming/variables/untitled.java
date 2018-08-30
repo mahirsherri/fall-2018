860893bdfb4e69d96a852002665b1e90e97cafd6
CybertekSchool <github@cybertekschool.com>
2018-08-29 22:19:37 -0400

    variables

---
+++ java-programming/variables/untitled.java
@@ -0,0 +1,39 @@
+
+public class PrimitivesShow {
+	public static void main(String[] args) {
+		//byte variables
+		byte b1 = 100;
+		byte b2 = 127;
+
+		//short variables
+		short sh1 = 10000;
+		short sh2 = -23456;
+
+		//int variables
+		int i1 = 123000;
+		int i2 = -64554353;
+
+		//long variables
+		long l1 = -2352342342L;
+		long l2 = 34534534212l;
+
+		//float variables
+		float f1 = 5642324.65F;
+		float f2 = 1.1f;
+
+		//double variables
+		double d1 = -534543.23;
+		double d2 = 23423.1;
+
+		//char variable
+		char ch1 = 'A';
+		char ch2 = '%';
+
+		//boolean variables
+		boolean b1 = true;
+		boolean b2 = false;
+
+		
+
+	}
+}
