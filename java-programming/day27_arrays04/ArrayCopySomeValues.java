205db9a0f3849573882ee8a548ce5306d2038062
CybertekSchool <github@cybertekschool.com>
2018-10-10 22:10:15 -0400

    String array review, split method, Replit introduction

---
+++ java-programming/day27_arrays04/ArrayCopySomeValues.java
@@ -0,0 +1,38 @@
+package day27_arrays04;
+
+public class ArrayCopySomeValues {
+	public static void main(String[] args) {
+		String[] arr = {"zero", "one", "two","three","four","keys"};
+		
+		//count how many words have 'e' in the arr
+		int counter = 0;
+		for(String str : arr) {
+			
+			if(str.contains("e")) {
+				counter++;
+			}
+			
+		}
+		
+		System.out.println(counter);
+		//create array fewValues, with size of counter
+		String[] fewValues = new String[counter];
+		
+		//go over arr one more time and add the words with 'e' into fewValues
+		int i = 0;
+		
+		for(String each : arr) {
+			if(each.contains("e")) {
+				fewValues[i] = each;
+				i++;
+			}
+		}
+		
+		for( String val : fewValues) {
+			System.out.print(val + " ");
+		}
+		
+		
+
+	}
+}
---
+++ java-programming/day27_arrays04/ArrayCopySomeValuesV2.java
@@ -0,0 +1,36 @@
+package day27_arrays04;
+
+public class ArrayCopySomeValuesV2 {
+	public static void main(String[] args) {
+		String[] arr = {"zero", "one", "two","three","four","keys"};
+		
+		//count how many words have 'e' in the arr
+		int counter = 0;
+		for(int i=0; i<arr.length;i++) {
+			
+			if(arr[i].contains("e")) {
+				counter++;
+			}
+			
+		}
+		
+		System.out.println(counter);
+		//create array fewValues, with size of counter
+		String[] fewValues = new String[counter];
+		
+		//go over arr one more time and add the words with 'e' into fewValues
+		int i = 0;
+		
+		for(int j=0; j < arr.length; j++) {
+			if(arr[j].contains("e")) {
+				fewValues[i] = arr[j];
+				i++;
+			}
+		}
+		
+		for( String val : fewValues) {
+			System.out.print(val + " ");
+		}
+
+	}
+}
