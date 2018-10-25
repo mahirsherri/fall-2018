b5ab58dda20b51feb8488e3b16e2c7fd42a82218
CybertekSchool <github@cybertekschool.com>
2018-10-24 22:11:54 -0400

    day 35

---
+++ java-programming/day35/MethodsAndStuff.java
@@ -0,0 +1,37 @@
+package day35;
+
+public class MethodsAndStuff {
+	public static void main(String[] args) {
+		String str1 = "halloweenn";
+		String str2 = "halloweenns";
+
+		int count = numberOfOccurences(str1, str2);
+		System.out.println(count);
+	}
+
+	/*
+	 * given a string, write a method which returns how many times a certain string
+	 * appears on another string
+	 */
+	public static int numberOfOccurences(String str1, String str2) {
+		int count = 0;
+		for (int i = 0; i < str1.length() - str2.length() + 1; i++) {
+			String current = str1.substring(i, i + str2.length());
+			if (current.equals(str2)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	/*
+	 * delete utility will delete a given character from the given string
+	 * 
+	 */
+
+	public static String delete(String str1, String str2) {
+		String newOne = str1.replace(str2, "");
+		return newOne;
+	}
+
+}
