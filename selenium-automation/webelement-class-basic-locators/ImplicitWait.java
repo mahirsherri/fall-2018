7d54f67195c39bae7ef90651c4de083c5ad19473
marufjon <maruft@gmail.com>
2018-12-01 18:43:11 -0500

    WebElement class, basic locators

---
+++ selenium-automation/webelement-class-basic-locators/ImplicitWait.java
@@ -0,0 +1,32 @@
+package com.cybertek;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ImplicitWait {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "/Users/cybertekschool/Documents/selenium dependencies/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        // this line makes the findElement method wait for some time when it cannot find an element
+        // findElement method will keep trying to locate the element during the given duration
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.get("http://www.cheapfansedge.info");
+
+        driver.findElement(By.tagName("inpsdfsdfut")).sendKeys("asdfasdfd");
+
+        // HARD CODED WAIT
+
+        Thread.sleep(5000);
+
+        driver.get("https://google.com");
+    }
+}
