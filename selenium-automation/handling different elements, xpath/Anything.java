181ddbf60ec6755a7edc840ea378a3844e0a442c
marufjon <maruft@gmail.com>
2018-12-09 08:14:26 -0500

    dropdown, checkboxes, radio buttons and xpath

---
+++ selenium-automation/handling different elements, xpath/Anything.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import com.github.javafaker.Faker;
+
+public class Anything {
+    public static void main(String[] args) {
+        Faker faker = new Faker();
+
+        System.out.println(
+                faker.name().fullName());
+
+    }
+}
